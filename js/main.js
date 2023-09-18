bff42333cf3a6304be744a44d3e55cd1fe39b1d9
Lucky <swordslayer07@gmail.com>
2023-09-18 15:58:49 -0600

    atomics update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function elementOut() {
+
+}
+
+/*function background() {
+    let backgroundC = window.getComputedStyle(document.body,null).getPropertyValue('background-color');
+    if (backgroundC == 'rgb(255, 255, 255)') {
+        console.log(true);
+        document.getElementById('body').style.cssText = 'background-color: #1a1a1a;';
+        document.getElementById('mainHeader').style.color = '#fff'
+        document.getElementById('themeBtn').innerHTML = '<i class="fa-solid fa-sun"></i> Day'
+    } else {
+        document.getElementById('body').style.cssText = 'background-color: #fff;';
+        document.getElementById('mainHeader').style.color = '#222222'
+        document.getElementById('themeBtn').innerHTML = '<i class="fa-solid fa-moon"></i> Night'
+    }
+}*/
