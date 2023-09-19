773582d04b984e977c2b7d30d6af8363f5f1a4e9
Lucky <68414136+Raidlucky@users.noreply.github.com>
2023-09-18 18:38:47 -0700

    periodic update

--- js/main.js
+++ js/main.js
@@ -15,3 +15,43 @@ function elementOut() {
         document.getElementById('themeBtn').innerHTML = '<i class="fa-solid fa-moon"></i> Night'
     }
 }*/
+
+function mini() {
+    let sidebar = document.getElementById('sidebar');
+    let sub = document.getElementById('subside');
+    let mini = document.getElementById('minimize');
+    let main = document.getElementById('main');
+    sidebar.style.cssText = 'width: 70px;';
+    subside.style.display = 'none';
+    mini.style.display = 'none';
+    main.style.paddingLeft = '70px';
+    document.getElementById('home').innerHTML = '<i class="fa-solid fa-house"></i>';
+    document.getElementById('home').style.width = '20px';
+    document.getElementById('table').innerHTML = '<i class="fa-solid fa-atom"></i>';
+    document.getElementById('table').style.width = '20px';
+    document.getElementById('info').innerHTML = '<i class="fa-solid fa-circle-info"></i>';
+    document.getElementById('info').style.width = '20px';
+    document.getElementById('history').innerHTML = '<i class="fa-solid fa-landmark-dome"></i>';
+    document.getElementById('history').style.width = '20px';
+    document.getElementById('expand').style.display = 'flex'
+}
+
+function expand() {
+    let sidebar = document.getElementById('sidebar');
+    let sub = document.getElementById('subside');
+    let mini = document.getElementById('minimize');
+    let main = document.getElementById('main');
+    sidebar.style.cssText = 'width: 230px;';
+    subside.style.display = 'block';
+    mini.style.display = 'flex';
+    main.style.paddingLeft = '230px';
+    document.getElementById('home').innerHTML = '<i class="fa-solid fa-house"></i><p style="margin: 0px; margin-left: 10px;">Home</p>';
+    document.getElementById('home').style.width = '200px';
+    document.getElementById('table').innerHTML = '<i class="fa-solid fa-atom"></i><p style="margin: 0px; margin-left: 10px;">Periodic Table</p>';
+    document.getElementById('table').style.width = '200px';
+    document.getElementById('info').innerHTML = '<i class="fa-solid fa-circle-info"></i><p style="margin: 0px; margin-left: 10px;">Info</p>';
+    document.getElementById('info').style.width = '200px';
+    document.getElementById('history').innerHTML = '<i class="fa-solid fa-landmark-dome"></i><p style="margin: 0px; margin-left: 10px;">History</p>';
+    document.getElementById('history').style.width = '200px';
+    document.getElementById('expand').style.display = 'none'
+}
