ccec2badad10fc0cfe7d81f7c9954166cdaa2047
Lucky <swordslayer07@gmail.com>
2023-09-26 16:12:52 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-function elementOut() {
-
-}
-
-/*function background() {
-    let backgroundC = window.getComputedStyle(document.body,null).getPropertyValue('background-color');
-    if (backgroundC == 'rgb(255, 255, 255)') {
-        console.log(true);
-        document.getElementById('body').style.cssText = 'background-color: #1a1a1a;';
-        document.getElementById('mainHeader').style.color = '#fff'
-        document.getElementById('themeBtn').innerHTML = '<i class="fa-solid fa-sun"></i> Day'
-    } else {
-        document.getElementById('body').style.cssText = 'background-color: #fff;';
-        document.getElementById('mainHeader').style.color = '#222222'
-        document.getElementById('themeBtn').innerHTML = '<i class="fa-solid fa-moon"></i> Night'
-    }
-}*/
-
 function mini() {
     let sidebar = document.getElementById('sidebar');
     let sub = document.getElementById('subside');
@@ -33,7 +15,7 @@ function mini() {
     document.getElementById('info').style.width = '17px';
     document.getElementById('history').innerHTML = '<i class="fa-solid fa-landmark-dome"></i>';
     document.getElementById('history').style.width = '17px';
-    document.getElementById('expand').style.display = 'flex'
+    document.getElementById('expand').style.display = 'flex';
 }
 
 function expand() {
@@ -53,7 +35,7 @@ function expand() {
     document.getElementById('info').style.width = '200px';
     document.getElementById('history').innerHTML = '<i class="fa-solid fa-landmark-dome"></i><p style="margin: 0px; margin-left: 10px;">History</p>';
     document.getElementById('history').style.width = '200px';
-    document.getElementById('expand').style.display = 'none'
+    document.getElementById('expand').style.display = 'none';
 }
 
 async function index(num) {
@@ -118,7 +100,7 @@ function eleTheme(data, index) {
 
 function closeSide() {
     document.getElementById('sideInfo').style.display = 'none';
-    document.getElementById('sideInfo').style.transform = 'translateX(0px)'
+    document.getElementById('sideInfo').style.transform = 'translateX(0px)';
 }
 
-mini()
+mini();
